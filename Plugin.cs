8e34cac914b1c07293fbfd01cdec0e99a2554100
AurosTV <aurosyt0@gmail.com>
2018-10-07 21:28:57 -0400

    Added Tracking Loss Detector
    Added Tracking Loss Detector

--- Plugin.cs
+++ Plugin.cs
@@ -1,18 +1,19 @@
 ﻿using IllusionPlugin;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.Linq;
 
 namespace AurosAutoPause
 {
     public class Plugin : IPlugin
     {
         public string Name => "Auros's AutoPause";
-        public string Version => "0.0.4";
+        public string Version => "0.1.0";
         public void OnApplicationStart()
         {
             SceneManager.activeSceneChanged += SceneManagerOnActiveSceneChanged;
             SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+            
         }
 
         private void SceneManagerOnActiveSceneChanged(Scene arg0, Scene arg1)
@@ -31,19 +32,76 @@ namespace AurosAutoPause
 
         public void OnLevelWasLoaded(int level)
         {
+            GameplayManager PMM = Resources.FindObjectsOfTypeAll<GameplayManager>().FirstOrDefault();
+            if (PMM != null)
+            {
+                PMM.ResumeFromPause();
+            }
         }
 
         public void OnLevelWasInitialized(int level)
         {
         }
 
+        public float despacito;
+        //ignore that ^^^
+
+        private float nextActionTime = 0.0f;
+        public float period = 0.1f;
+
+        private static Vector3 text;
+        private static Vector3 text2;
+
         public void OnUpdate()
         {
-            float fps = 1.0f / Time.deltaTime;
-            if (fps < 40.0)
+            if (Time.time > nextActionTime)
             {
-                GameplayManager PMM = (GameplayManager)Resources.FindObjectsOfTypeAll(typeof(GameplayManager))[0];
-                PMM.Pause();
+                nextActionTime += period;
+
+                PlayerController HEL = Resources.FindObjectsOfTypeAll<PlayerController>().FirstOrDefault();
+                if (HEL == null)
+                    return;
+                Saber SaberThatIsLeft = HEL.leftSaber;
+                Saber SaberThatIsRight = HEL.rightSaber;
+
+                Vector3 LeftSaberHandleLocation = SaberThatIsLeft.handlePos;
+                Vector3 RightSaberHandleLocation = SaberThatIsRight.handlePos;
+
+                GamePauseManager GMM = Resources.FindObjectsOfTypeAll<GamePauseManager>().FirstOrDefault();
+                if (GMM != null && GMM.pause == true)
+                {
+                    text = RightSaberHandleLocation;
+                    text2 = LeftSaberHandleLocation;
+                }
+                else
+                {
+                    //FPS CHECKER
+
+                    float fps = 1.0f / Time.deltaTime;
+                    if (fps < 40.0f)
+                    {
+                        GameplayManager PMM = Resources.FindObjectsOfTypeAll<GameplayManager>().FirstOrDefault();
+                        if (PMM != null)
+                        {
+                            PMM.Pause();
+                        }
+                    }
+
+                    //TRACKING DETECTOR
+
+                    if (text == LeftSaberHandleLocation || text2 == RightSaberHandleLocation)
+                    {
+                        GameplayManager PMM = Resources.FindObjectsOfTypeAll<GameplayManager>().FirstOrDefault();
+                        if (PMM != null)
+                        {
+                            PMM.Pause();
+                        }
+                    }
+
+                    text = LeftSaberHandleLocation;
+                    text2 = RightSaberHandleLocation;
+
+                }
             }
         }
 
